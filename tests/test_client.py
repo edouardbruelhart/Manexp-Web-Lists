d1fbca0d615004542a1966b530e6bfffb1b6e9e5
edouardbruelhart <edouard.brulhart@gmail.com>
2026-02-04 10:27:30 +0100

    updated test to match the new JsonClient

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,24 @@
-from manexp_web_lists.client import DownloadJsonClient
+from manexp_web_lists.client import JsonClient
 
 
 def test_download(tmp_path):
     url = "https://raw.githubusercontent.com/blw-ofag-ufag/blw-ogd-data/refs/heads/main/data/plant_varieties_in_switzerland.json"
-    file_path = tmp_path / "plant_varieties.json"
+    file_path = tmp_path / "crops.json"
 
-    client = DownloadJsonClient(url, file_path)
+    client = JsonClient(url, file_path)
     client.download_file()
 
     assert file_path.exists()
     assert file_path.stat().st_size > 0
+
+
+def test_load_file(tmp_path):
+    url = "https://raw.githubusercontent.com/blw-ofag-ufag/blw-ogd-data/refs/heads/main/data/plant_varieties_in_switzerland.json"
+    file_path = tmp_path / "crops.json"
+
+    client = JsonClient(url, file_path)
+    client.download_file()
+
+    variety = client.load_file()
+
+    assert variety is not None
